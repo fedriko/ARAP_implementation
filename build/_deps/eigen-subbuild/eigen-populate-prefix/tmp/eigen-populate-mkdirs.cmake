2230cf7938a854e750399dec7ba02f9db2b78e76
fedriko <166320114+fedriko@users.noreply.github.com>
2026-03-14 17:32:17 -0400

    oo
    ooo

---
+++ build/_deps/eigen-subbuild/eigen-populate-prefix/tmp/eigen-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-src"
+  "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-build"
+  "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix"
+  "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/tmp"
+  "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp"
+  "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src"
+  "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp"
+)
+
+set(configSubDirs Debug)
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/federico/My project (3)/ARAP_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <Eigen/Eigen> 
+void main(){
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+    Eigen::SparseMatrix<double> weights;
+    
+}
+
+Eigen::SparseMatrix<double> weights(const Eigen::MatrixXd & V,const Eigen::MatrixXi & F ){
+
+    int n = V.rows();
+    int f = F.rows();
+    Eigen::SparseMatrix<double> W(n,n);
+    std::vector<Eigen::Triplet<double>> triplets;
+    for(int i = 0; i<f; i++){
+
+        int i1 =F(i,0);
+        int i2 =F(i,1);
+        int i3 =F(i,2);
+
+        Eigen::Vector3d  v1 = V.row(i1);
+        Eigen::Vector3d  v2 = V.row(i2);
+        Eigen::Vector3d  v3 = V.row(i3);
+
+        double a1 = std::atan2((v2-v1).cross(v3-v1).norm() ,(v2-v1).dot(v3-v1)); 
+        double a2 = std::atan2((v1-v2).cross(v3-v2).norm() ,(v1-v2).dot(v3-v2)); 
+        double a3 = std::atan2((v1-v3).cross(v2-v3).norm() ,(v1-v3).dot(v2-v3)); 
+
+        double w1 = (1.0/2)*(std::cos(a1)/std::sin(a1));
+        double w2 = (1.0/2)*(std::cos(a2)/std::sin(a2));
+        double w3 = (1.0/2)*(std::cos(a3)/std::sin(a3));
+
+        triplets.push_back({i2,i3,w1});
+        triplets.push_back({i3,i2,w1});
+        
+        triplets.push_back({i1,i3,w2});
+        triplets.push_back({i3,i1,w2});
+
+        triplets.push_back({i1,i2,w3});
+        triplets.push_back({i2,i1,w3});
+    }
+    W.setFromTriplets(triplets.begin(), triplets.end()); 
+    return W;
+}
