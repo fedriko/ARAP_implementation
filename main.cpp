891df74039744069b09f86095aead4e615aa98f6
fedriko <166320114+fedriko@users.noreply.github.com>
2026-03-17 10:46:00 -0400

    yes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
 #include <Eigen/Eigen> 
-Eigen::SparseMatrix<double>  constructLaplace(Eigen::SparseMatrix<double> w){
-   Eigen::VectorXd sums = W_sparse * Eigen::VectorXd::Ones(W_sparse.cols());
-   Eigen::SparseMatrix<double> D_sparse = sums.asDiagonal();
+#include <igl/cotmatrix.h>
+#include <igl/adjacency_list.h>
+#include <igl/massmatrix.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/read_triangle_mesh.h>
+#include <igl/readOFF.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <Eigen/Core>
+Eigen::SparseMatrix<double, Eigen::RowMajor>  constructLaplace(const Eigen::SparseMatrix<double, Eigen::RowMajor> & w){
+    Eigen::VectorXd sums = w * Eigen::VectorXd::Ones(w.cols());
+
+    Eigen::SparseMatrix<double, Eigen::RowMajor> D_sparse(sums.size(), sums.size());
+    std::vector<Eigen::Triplet<double>> triplets;
+    triplets.reserve(sums.size());
+
+    for (int i = 0; i < sums.size(); ++i) {
+        triplets.emplace_back(i, i, sums[i]);
+    }
+
+    D_sparse.setFromTriplets(triplets.begin(), triplets.end());
    return D_sparse - w;
     
 }
-
-Eigen::SparseMatrix<double> constructWeights(const Eigen::MatrixXd & V,const Eigen::MatrixXi & F ){
+double cotan(const Eigen::Vector3d& a, const Eigen::Vector3d& b) {
+    double denom = a.cross(b).norm();
+    if (denom < 1e-12) return 0.0;
+    return a.dot(b) / denom;
+}
+Eigen::SparseMatrix<double, Eigen::RowMajor> constructWeights(const Eigen::MatrixXd & V,const Eigen::MatrixXi & F ){
 
     int n = V.rows();
     int f = F.rows();
-    Eigen::SparseMatrix<double> W(n,n);
+    Eigen::SparseMatrix<double, Eigen::RowMajor>  W(n,n);
     std::vector<Eigen::Triplet<double>> triplets;
     for(int i = 0; i<f; i++){
 
@@ -18,35 +39,114 @@ Eigen::SparseMatrix<double> constructWeights(const Eigen::MatrixXd & V,const Eig
         int i2 =F(i,1);
         int i3 =F(i,2);
 
-        Eigen::Vector3d  v1 = V.row(i1);
-        Eigen::Vector3d  v2 = V.row(i2);
-        Eigen::Vector3d  v3 = V.row(i3);
+        Eigen::Vector3d  v1 = V.row(i1).transpose();
+        Eigen::Vector3d  v2 = V.row(i2).transpose();
+        Eigen::Vector3d  v3 = V.row(i3).transpose();
 
-        double a1 = std::atan2((v2-v1).cross(v3-v1).norm() ,(v2-v1).dot(v3-v1)); 
-        double a2 = std::atan2((v1-v2).cross(v3-v2).norm() ,(v1-v2).dot(v3-v2)); 
-        double a3 = std::atan2((v1-v3).cross(v2-v3).norm() ,(v1-v3).dot(v2-v3)); 
 
-        double w1 = (1.0/2)*(std::cos(a1)/std::sin(a1));
-        double w2 = (1.0/2)*(std::cos(a2)/std::sin(a2));
-        double w3 = (1.0/2)*(std::cos(a3)/std::sin(a3));
+       double w1 = 0.5 * cotan(v2 - v1, v3 - v1);
+       double w2 = 0.5 * cotan(v1 - v2, v3 - v2);
+       double w3 = 0.5 * cotan(v1 - v3, v2 - v3);
 
-        triplets.push_back({i2,i3,w1});
-        triplets.push_back({i3,i2,w1});
+        triplets.emplace_back(i2,i3,w1);
+        triplets.emplace_back(i3,i2,w1);
         
-        triplets.push_back({i1,i3,w2});
-        triplets.push_back({i3,i1,w2});
+        triplets.emplace_back(i1,i3,w2);
+        triplets.emplace_back(i3,i1,w2);
 
-        triplets.push_back({i1,i2,w3});
-        triplets.push_back({i2,i1,w3});
+        triplets.emplace_back(i1,i2,w3);
+        triplets.emplace_back(i2,i1,w3);
     }
     W.setFromTriplets(triplets.begin(), triplets.end()); 
     return W;
 }
 
-void main(){
-    Eigen::MatrixXd V;
+Eigen::Matrix3d computeRotation(int i, const Eigen::MatrixXd & V_1, const Eigen::MatrixXd & V_2, const Eigen::SparseMatrix<double, Eigen::RowMajor> & w){
+    
+    Eigen::Matrix3d Si = Eigen::Matrix3d::Zero();
+    Eigen::Vector3d vi1 = V_1.row(i).transpose();
+    Eigen::Vector3d vi2 = V_2.row(i).transpose();
+
+    for (Eigen::SparseMatrix<double, Eigen::RowMajor> ::InnerIterator it(w, i); it; ++it) {
+        int j = it.col();
+
+        Eigen::Vector3d e1 = V_1.row(j).transpose() - vi1;
+        Eigen::Vector3d e2 = V_2.row(j).transpose() - vi2;
+
+        Si += it.value()*e1*e2.transpose();
+        
+    }
+
+    Eigen::JacobiSVD<Eigen::Matrix3d> svd(Si, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::Matrix3d U = svd.matrixU();
+    Eigen::Matrix3d V = svd.matrixV();
+    // check determinant 
+    return V*U.transpose();
+}
+
+std::vector<Eigen::Vector3d> computeB(const Eigen::MatrixXd & V_1, const Eigen::SparseMatrix<double, Eigen::RowMajor> & W, const std::vector<Eigen::Matrix3d>& R){
+   
+    int n = V_1.rows();
+    std::vector<Eigen::Vector3d> b(n, Eigen::Vector3d::Zero());
+    
+    for(int i = 0; i < n; i++){
+            const Eigen::Matrix3d & R_i = R[i];
+            Eigen::Vector3d p_i = V_1.row(i).transpose();
+         for (Eigen::SparseMatrix<double, Eigen::RowMajor> ::InnerIterator it(W, i); it; ++it) {
+            int j = it.col();
+            Eigen::Vector3d p_j = V_1.row(j).transpose();
+            b[i] += 0.5*it.value()*(R_i+R[j])*(p_i-p_j);
+         }
+    }
+    return b;
+}
+
+int main(){
+    Eigen::MatrixXd V_1;
+    Eigen::MatrixXd V_2;
     Eigen::MatrixXi F;
-    Eigen::SparseMatrix<double> weights = ; constructWeights(V,F);
+
+    igl::readOFF("dino.off",V_1,F);
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V, F);
+    viewer.launch();
+    int n = V_1.rows();
+    Eigen::SparseMatrix<double, Eigen::RowMajor> W = constructWeights(V_1,F);
+
+    auto L = constructLaplace(W);
+    Eigen::SimplicialLLT<Eigen::SparseMatrix<double, Eigen::RowMajor>> solver;
+    solver.compute(L);
+
+
+
+
+    for(int j = 0; j < 3; j++){
+
+        std::vector<Eigen::Matrix3d> R;
+        R.resize(n);
+
+        for(int i=0; i<n; i++){
+            R[i] = computeRotation(i,V_1,V_2,W);
+        }
+        auto b = computeB(V_1,W,R);
     
-    
+        Eigen::VectorXd bx(n),by(n),bz(n);
+        for(int i = 0; i < n; i++){
+            bx(i) = b[i](0);
+            by(i) = b[i](1);
+            bz(i) = b[i](2);
+
+        }
+        Eigen::VectorXd x = solver.solve(bx);
+        Eigen::VectorXd y = solver.solve(by);
+        Eigen::VectorXd z = solver.solve(bz);
+
+        for(int i = 0; i < n; i++) {
+            V_2(i, 0) = x(i);
+            V_2(i, 1) = y(i);
+            V_2(i, 2) = z(i);
+        }
+    }
+   
+    return 0;
 }
